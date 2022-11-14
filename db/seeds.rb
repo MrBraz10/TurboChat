74048466f554783c11d72744cb530e28484b8bd8
MrBraz10 <nekita10@mail.ru>
2022-11-14 21:33:08 +0300

    recent message in chatroom list

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+nekita = User.create(email: 'nekita10@mail.ru',
+                     password: 'password',
+                     password_confirmation: 'password',
+                     role: 'admin')
+User.create(email: 'leva1570@yandex.ru',
+            password: 'password',
+            password_confirmation: 'password')
+User.create(email: 'test@test.ru',
+            password: 'password',
+            password_confirmation: 'password')
+nekita.joined_rooms << Room.create(name: 'General', is_private: false)
+nekita.joined_rooms << Room.create(name: 'Testing', is_private: false)
